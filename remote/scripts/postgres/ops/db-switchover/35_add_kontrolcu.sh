e24a7e28b55036565999db4052d35f3086837686
yusufbulentavci <avci.yusuf@gmail.com>
2022-09-30 14:50:46 +0300

    .

--- remote/scripts/postgres/ops/db-switchover/35_add_kontrolcu.sh
+++ remote/scripts/postgres/ops/db-switchover/35_add_kontrolcu.sh
@@ -14,9 +14,9 @@ echo "auth_user=$auth_user"
 psql $publisher_db << EOF
 CREATE OR REPLACE FUNCTION public.kullanici_bul(uname TEXT) RETURNS
 TABLE (usename name, passwd text) as
-$$
+\$\$
   SELECT usename, passwd FROM pg_shadow WHERE usename=\$1;
-$$
+\$\$
 LANGUAGE sql SECURITY DEFINER;
 GRANT EXECUTE ON FUNCTION public.kullanici_bul to $auth_user;
 grant USAGE ON SCHEMA public to $auth_user;
